6c47068cdcacf8c92868dd7b3910823700eb3905
EvilDrifter <klenov.em@gmail.com>
2020-08-16 14:32:25 +0300

    lesson_5: render items from DB

--- src/app/components/house-item/house-item.component.ts
+++ src/app/components/house-item/house-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { House } from 'src/app/models/house';
 
 @Component({
   selector: 'app-house-item',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./house-item.component.css']
 })
 export class HouseItemComponent implements OnInit {
-
+  @Input() info: House;
   constructor() { }
 
   ngOnInit() {
